9758d645bd62cd0cd62bb0cccdc6ca5ce37663bc
defendimattia <defendi.mattia92@gmail.com>
2025-06-25 10:29:52 +0200

    Refactor Product constructor to accept parameters for name, description, and price

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -9,8 +9,16 @@ public class Product {
     float price;
     int iva;
 
-    Product() {
+    Product(String name, String description, float price) {
         Random randomNum = new Random();
-        this.code = String.format("%06d",randomNum.nextInt(1000000));
+        this.code = String.format("%06d", randomNum.nextInt(1000000));
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = 22;
+    }
+
+    void showBasePrice() {
+        System.out.println(this.price);
     }
 }
