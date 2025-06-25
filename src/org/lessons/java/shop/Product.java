f59fae1b4f333c71db03a911b82d0a3b50563f18
defendimattia <defendi.mattia92@gmail.com>
2025-06-25 16:14:26 +0200

    Add validation to setter methods in Product class for name, description, price, and IVA

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -37,12 +37,30 @@ public class Product {
         return this.name;
     }
 
+    public void setName(String newName) {
+        if (newName != null && !newName.isBlank()) {
+            this.name = newName;
+        }
+    }
+
     public String getDescription() {
         return this.description;
     }
 
+    public void setDescription(String newDescription) {
+        if (newDescription != null && !newDescription.isBlank()) {
+            this.description = newDescription;
+        }
+    }
+
     public BigDecimal getBasePrice() {
         return this.price;
+    }
+
+    public void setBasePrice(BigDecimal newBasePrice) {
+        if (newBasePrice != null && newBasePrice.compareTo(BigDecimal.ZERO) > 0) {
+            this.price = newBasePrice;
+        }
     }
 
     public BigDecimal getIva() {
@@ -51,6 +69,12 @@ public class Product {
         }
 
         return null;
+    }
+
+    public void setIva(BigDecimal newIva) {
+        if (newIva != null && newIva.compareTo(BigDecimal.ZERO) >= 0) {
+            this.iva = newIva;
+        }
     }
 
     public BigDecimal getFullPrice() {
